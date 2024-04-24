76e897019e9a56b0323d7761db53d442016aa813
tanyaporseva <tporseva@inbox.ru>
2024-04-24 13:05:29 +0300

    frontend

--- developer/src/main/resources/static/main.js
+++ developer/src/main/resources/static/main.js
@@ -11,10 +11,12 @@ function createBuildings(buildings) {
     let buildingsHTML = '';
 
     for (const building of buildings) {
+        let minutesDeclension = getMinutesDeclension(building.distance);
+
         buildingsHTML += `
             <div class="complex">
                 <span class="complex__name">ЖК ${building.title}</span>
-                <span class="complex__metro">${building.distance} минут до ${building.location}</span>
+                <span class="complex__metro">${building.distance} ${minutesDeclension} до ${building.location}</span>
                 <div class="complex__image">
                     <img src="${building.image}"/>
                 </div>
@@ -23,4 +25,14 @@ function createBuildings(buildings) {
     }
 
     buildingsContainer.innerHTML = buildingsHTML;
+}
+
+function getMinutesDeclension(number) {
+    if (number % 10 === 1 && number % 100 !== 11) {
+        return "минута";
+    } else if ([2, 3, 4].includes(number % 10) && ![12, 13, 14].includes(number % 100)) {
+        return "минуты";
+    } else {
+        return "минут";
+    }
 }
